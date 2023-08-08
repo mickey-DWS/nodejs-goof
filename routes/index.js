dce7c3ba8d51f515c99ac8e0bb85f098fc078eb3
mickey-DWS <miki.kuronuma@mmmcorp.co.jp>
2023-08-08 18:36:13 +0900

    脆弱性の追加

--- routes/index.js
+++ routes/index.js
@@ -88,6 +88,23 @@ exports.loginHandler2 = function (req, res, next) {
   }
 };
 
+exports.loginHandler3 = function (req, res, next) {
+  if (validator.isEmail(req.body.username)) {
+    User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
+      if (users.length > 0) {
+        const redirectPage = req.body.redirectPage
+        const session = req.session
+        const username = req.body.username
+        return adminLoginSuccess(redirectPage, session, username, res)
+      } else {
+        return res.status(401).send()
+      }
+    });
+  } else {
+    return res.status(401).send()
+  }
+};
+
 function adminLoginSuccess(redirectPage, session, username, res) {
   session.loggedIn = 1
 
